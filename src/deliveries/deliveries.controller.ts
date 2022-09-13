38bb3e730603a7817961dc8fdcfdd2a5638eb391
ParkSuJeong74 <sujeong9158@gmail.com>
2022-09-14 00:34:54 +0900

    feat: 주문건에 대하여 발송 처리 api 생성

--- src/deliveries/deliveries.controller.ts
+++ src/deliveries/deliveries.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { DeliveriesService } from './deliveries.service';
+import { StartDeliveryDto } from './dto';
 
 @Controller('deliveries')
 export class DeliveriesController {
   constructor(private readonly deliveriesService: DeliveriesService) {}
+
+  @HttpCode(201)
+  @Post()
+  async startDelivery(@Body() startDeliveryDto: StartDeliveryDto) {
+    return await this.deliveriesService.startDelivery(startDeliveryDto);
+  }
+
+  // 배송 상태 업데이트
 }
--- src/deliveries/deliveries.service.ts
+++ src/deliveries/deliveries.service.ts
@@ -1,7 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { StartDeliveryDto } from './dto';
 
 @Injectable()
 export class DeliveriesService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async startDelivery(startDeliveryDto: StartDeliveryDto) {
+    try {
+      const delivery = await this.prisma.delivery.create({
+        data: { id: startDeliveryDto.id },
+      });
+      return delivery;
+    } catch (err) {
+      throw new NotFoundException(
+        '주문건에 대하여 발송처리를 완료하지 못했습니다.',
+      );
+    }
+  }
 }
