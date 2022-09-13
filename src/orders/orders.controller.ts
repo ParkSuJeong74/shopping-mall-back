b553123a088226b22362c8d7f0bf3425a3e16209
ParkSuJeong74 <sujeong9158@gmail.com>
2022-09-13 23:01:25 +0900

    feat: 제품 주문 내역 열람 api에 swagger 적용

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Logger, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpCode, Logger, Param } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Order } from '@prisma/client';
 import { OrdersService } from './orders.service';
 
@@ -9,7 +9,17 @@ export class OrdersController {
   private readonly logger = new Logger(`OrdersController`);
   constructor(private readonly ordersService: OrdersService) {}
 
+  @HttpCode(200)
   @Get(':id')
+  @ApiOperation({
+    summary: '제품 주문 내역 열람 API',
+    description: '제품 주문 내역을 열람한다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '제품 주문 내역 열람 성공',
+  })
+  @ApiParam({ name: 'id', required: true, description: '주문 아이디' })
   async getOrders(@Param('id') id: number): Promise<Order> {
     return await this.ordersService.getOrders(id);
   }
