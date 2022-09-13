ece02f466fa8e95adec2e6bad5f24ce689b6ac1b
ParkSuJeong74 <sujeong9158@gmail.com>
2022-09-14 00:39:06 +0900

    feat: 주문건에 대하여 발송처리 swagger 적용

--- src/deliveries/deliveries.controller.ts
+++ src/deliveries/deliveries.controller.ts
@@ -1,13 +1,24 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { DeliveriesService } from './deliveries.service';
 import { StartDeliveryDto } from './dto';
 
+@ApiTags('Deliveries API')
 @Controller('deliveries')
 export class DeliveriesController {
   constructor(private readonly deliveriesService: DeliveriesService) {}
 
   @HttpCode(201)
   @Post()
+  @ApiOperation({
+    summary: '제품 발송 처리 API',
+    description: '제품 발송을 배송중으로 처리한다.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: '제품 발송 처리 성공',
+  })
+  @ApiBody({ type: StartDeliveryDto })
   async startDelivery(@Body() startDeliveryDto: StartDeliveryDto) {
     return await this.deliveriesService.startDelivery(startDeliveryDto);
   }
